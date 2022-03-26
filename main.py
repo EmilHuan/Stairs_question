91fdd8f1e2195dac4f77fde7a9fb2a6ee36f73d9
EmilHuan <s93yoyo695072@gmail.com>
2022-03-26 12:12:25 +0800

    redactor: sample oop version. Stairs_question.py, main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # 匯入 Stairs_question.py
-import Stairs_question as Stairs
+from Stairs_question import stairs_cal
 
 # 主程式用 while True 包覆，出現例外 (Exception) 時可重新讓使用者輸入
 while True:
     # 例外處理
     try:
         # 樓梯總階數
-        stairs = eval(input("樓梯總階數："))
+        stairs = eval(input("輸入樓梯總階數："))
 
     # SyntaxError 例外，輸入值出現特殊字元時回傳錯誤訊息
     except SyntaxError:
@@ -20,12 +20,14 @@ while True:
 
     # 如例外檢查通過，執行 Date_function.py 檔案裡的函數 print_date
     else:
-        answer = Stairs.ways_cal(stairs)
+        ways = stairs_cal()
+        answer = ways.ways_cal(stairs)
         print("共 {} 種不同的上樓方式".format(answer))
 
         # 詢問使用者是否要繼續計算其它階梯數
         next_print = input("要計算其它階梯數嗎？(繼續計算請輸入 y，結束計算請按任意鍵)：")
         if next_print.lower().strip() == "y":
+            print()
             pass
         else:
             print("感謝您的使用！")
